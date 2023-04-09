7e131586ff8b5a82962e3783fbd0af03a12a634e
sekomum <129589748+sekomum@users.noreply.github.com>
2023-04-09 12:51:09 +0000

    [add] bookers5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,14 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to'/top'
+  end
+    
 
   def index
   end
@@ -9,5 +17,10 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    parames.require(:book).permit(title, body)
   end
 end
